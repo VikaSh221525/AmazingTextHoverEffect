7a22a909726f239ad4f074e82740f520178c4393
VikaSh221525 <vikassharma221525@gmail.com>
2025-05-08 20:30:31 +0530

    Sheryjs

---
+++ EffectJs.js
@@ -0,0 +1,15 @@
+
+// document.addEventListener("mousemove", function(dets){
+//     gsap.to("#cursor", {
+//         left: dets.x+27,
+//         top: dets.y-12,
+//         duration: 0.3, // smoothness
+//         ease: "power2.out" 
+//     });
+// })
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     Shery.mouseFollower();
+// });
+// Shery.mouseFollower();
+console.log(Shery);
